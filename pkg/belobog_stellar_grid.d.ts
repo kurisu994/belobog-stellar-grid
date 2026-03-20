d6c1f128c5df665da5938aa796b045e0d69b20f6
Kurisu <makise_kurisuu@outlook.jp>
2026-03-20 08:45:03 +0000

    🚀 部署示例页面和 WASM 产物 9b92025391a263f90f6c4ab34eeb18c55fb8b96d

--- pkg/belobog_stellar_grid.d.ts
+++ pkg/belobog_stellar_grid.d.ts
@@ -393,10 +393,10 @@ export interface InitOutput {
     readonly parseExcelToHtml: (a: number, b: number, c: any) => [number, number, number];
     readonly parseExcelToJson: (a: number, b: number, c: any) => [number, number, number];
     readonly wasm_bindgen__closure__destroy__hb1f1e222d193ce94: (a: number, b: number) => void;
-    readonly wasm_bindgen__closure__destroy__hc8a49241b11bcdf0: (a: number, b: number) => void;
+    readonly wasm_bindgen__closure__destroy__hdabd884f38b69116: (a: number, b: number) => void;
     readonly wasm_bindgen__convert__closures_____invoke__h4c21a5017d209281: (a: number, b: number, c: any) => [number, number];
     readonly wasm_bindgen__convert__closures_____invoke__h00d297b34697abe8: (a: number, b: number, c: any, d: any) => void;
-    readonly wasm_bindgen__convert__closures_____invoke__h13f4fda92ac6bfee: (a: number, b: number) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h1bdbb997274fbac9: (a: number, b: number) => void;
     readonly __wbindgen_malloc: (a: number, b: number) => number;
     readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
     readonly __wbindgen_exn_store: (a: number) => void;
--- pkg/belobog_stellar_grid.js
+++ pkg/belobog_stellar_grid.js
@@ -857,7 +857,7 @@ function __wbg_get_imports() {
         },
         __wbindgen_cast_0000000000000002: function(arg0, arg1) {
             // Cast intrinsic for `Closure(Closure { dtor_idx: 3, function: Function { arguments: [], shim_idx: 4, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
-            const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__hc8a49241b11bcdf0, wasm_bindgen__convert__closures_____invoke__h13f4fda92ac6bfee);
+            const ret = makeMutClosure(arg0, arg1, wasm.wasm_bindgen__closure__destroy__hdabd884f38b69116, wasm_bindgen__convert__closures_____invoke__h1bdbb997274fbac9);
             return ret;
         },
         __wbindgen_cast_0000000000000003: function(arg0) {
@@ -886,8 +886,8 @@ function __wbg_get_imports() {
     };
 }
 
-function wasm_bindgen__convert__closures_____invoke__h13f4fda92ac6bfee(arg0, arg1) {
-    wasm.wasm_bindgen__convert__closures_____invoke__h13f4fda92ac6bfee(arg0, arg1);
+function wasm_bindgen__convert__closures_____invoke__h1bdbb997274fbac9(arg0, arg1) {
+    wasm.wasm_bindgen__convert__closures_____invoke__h1bdbb997274fbac9(arg0, arg1);
 }
 
 function wasm_bindgen__convert__closures_____invoke__h4c21a5017d209281(arg0, arg1, arg2) {
